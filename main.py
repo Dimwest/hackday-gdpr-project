8c6234bc9bdbdd8b87714ae002174d1e067a3700
Dimwest <dimwestdev@gmail.com>
2019-03-07 12:05:24 +0100

    First steps

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pyspark
-from pyspark.sql.functions import udf
 
+# Script parameters to move to configuration file
 TEST_DIR = './test_data/*'
+MULTILINE = False
+TARGET_UUIDS = ['f8ae431d23c1af07b7010a20b96bbf62', '856d3a356a293db4ace7bf4ba0a90e6f']
+TARGET_FIELDS = ['email', 'country', 'meta.city', 'meta.ipv4']
 
-sc = pyspark.SparkContext()
-sqlContext = pyspark.SQLContext(sc)
+if __name__ == '__main__':
 
-# Reading json data into a dataframe
-df = sqlContext.read.json(TEST_DIR)
+    # Creating Spark SQLContext
+    sc = pyspark.SparkContext()
+    sqlContext = pyspark.SQLContext(sc)
 
-df.show(5)
+    # Reading json data into a dataframe
+    df = sqlContext.read.json(TEST_DIR, multiLine=MULTILINE)
+
+    df.select('uuid').show(50, truncate=False)
+    df.printSchema()
